75781b9ff1bfaf3c948665751b4dfb5efc3a9cb8
Iain Campbell <hey@iainmc.ca>
2022-08-08 10:44:05 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ $SPIN ]; then
-  ln -sf ~/dotfiles/zshrc_imports ~/.zshrc_imports
   ln -sf ~/dotfiles/zshrc_spin ~/.zshrc
 else
-  ln -sf ./.dotfiles/zshrc_imports ~/.zshrc_imports
   ln -sf ./.dotfiles/zshrc ~/.zshrc
 fi
